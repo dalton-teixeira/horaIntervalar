d469dc80acda6c78f2e010ce60d99fc66b3539ba
dalton <dalton.silveira@gmail.com>
2017-07-10 22:43:39 -0300

    adding first test in order to test the diif of minutes between start and date time

--- horaIntervalarWeb/HoraIntervalar.js
+++ horaIntervalarWeb/HoraIntervalar.js
@@ -1,5 +1,15 @@
 ﻿class HoraIntervalar {
-    calcInterval(interval) {
-        return interval[1] - interval[0];
+    /**
+     * /
+     * @param {Date} start
+     * @param {Date} end
+     * @returns {int} minutes
+     */
+    calcInterval(start, end) {
+        var diff = end - start;
+        //remove seconds and milleseconds repectively
+        var result = (diff / 60) / 1000
+        return result;
     }
- }
+}
+module.exports = HoraIntervalar;
--- horaIntervalarWeb/test/HoraIntervalarTest.js
+++ horaIntervalarWeb/test/HoraIntervalarTest.js
@@ -1,8 +1,16 @@
 ﻿var assert = require('assert');
+var HoraIntervalar = require('../horaIntervalar.js');
 describe('HoraIntervalar', function () {
     describe('#CalcInterval()', function () {
         it('should return the interval difference', function () {
-            assert.equal(1, 1);
+            //arrange
+            var sut = new HoraIntervalar();
+            var start = new Date(2017, 0, 24, 10, 33);
+            var end =   new Date(2017, 0, 24, 11, 33);
+            //act
+            var result = sut.calcInterval(start, end);
+            //assert
+            assert.equal(60, result);
         });
     });
 });
